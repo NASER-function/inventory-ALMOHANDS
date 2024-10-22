8baebfd64f17069ea6a75997d4cb2ad637ba865d
NASER <naserfunction@gmail.com>
2024-10-22 15:14:15 +0300

    Add files via upload

---
+++ date.js
@@ -0,0 +1,8 @@
+function updateDate() {
+    const today = new Date();
+    const day = String(today.getDate()).padStart(2, '0');
+    const month = String(today.getMonth() + 1).padStart(2, '0'); // January is 0!
+    const year = today.getFullYear();
+    document.getElementById('date').textContent = `${day}/${month}/${year}`;
+}
+updateDate();
